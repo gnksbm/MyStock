1cc840ac2ec293a2cada156a1d2ca244ab36927a
Geonseob Kim <109283556+gnksbm@users.noreply.github.com>
2024-03-05 21:42:40 +0900

    [Fix] CoreDataService CRUD 로직 수정

--- Projects/CoreDataService/Sources/DefaultCoreDataService.swift
+++ Projects/CoreDataService/Sources/DefaultCoreDataService.swift
@@ -25,19 +25,16 @@ public final class DefaultCoreDataService: CoreDataService {
     }
     
     public func fetch<T: CoreDataStorable>(type: T.Type) throws -> [T] {
-        let request = NSFetchRequest<NSManagedObject>(
-            entityName: "\(type)MO"
-        )
         do {
-            return try self.container.viewContext.fetch(request)
+            return try fetchMO(type: type)
                 .compactMap { $0 as? CoreDataModelObject }
-                .compactMap { $0.toEntity as? T }
+                .compactMap { $0.toDomain as? T }
         } catch {
             throw error
         }
     }
     
-    public func save<T: CoreDataStorable>(data: T) {
+    public func save<T: CoreDataStorable>(data: T) throws {
         let object = NSEntityDescription.insertNewObject(
             forEntityName: "\(type(of: data))MO",
             into: container.viewContext
@@ -45,7 +42,7 @@ public final class DefaultCoreDataService: CoreDataService {
         let mirror = Mirror(reflecting: data)
         mirror.children.forEach { key, value in
             guard let key,
-            let propertyName = String(describing: key)
+                  let propertyName = String(describing: key)
                 .split(separator: ".")
                 .last
             else { return }
@@ -55,62 +52,69 @@ public final class DefaultCoreDataService: CoreDataService {
             try container.viewContext.save()
         } catch {
             container.viewContext.rollback()
+            throw error
         }
     }
     
-    public func update(data: some CoreDataStorable) {
-        guard let entity = NSEntityDescription.entity(
-            forEntityName: "\(type(of: data))MO",
-            in: container.viewContext
-        )
-        else { return }
-        let object = NSManagedObject(
-            entity: entity,
-            insertInto: container.viewContext
-        )
-        let mirror = Mirror(reflecting: data)
-        mirror.children.forEach { key, value in
-            guard let key,
-            let propertyName = String(describing: key)
-                .split(separator: ".")
-                .last
-            else { return }
-            object.setValue(
-                value,
-                forKey: String(propertyName)
-            )
-        }
-        if container.viewContext.hasChanges {
-            do {
-                try container.viewContext.save()
-            } catch {
-                container.viewContext.rollback()
+    public func update<T: CoreDataStorable, U>(
+        data: T,
+        uniqueKeyPath: KeyPath<T, U>
+    ) throws {
+        do {
+            let mo = try fetchMO(type: type(of: data))
+            var object = mo.first { object in
+                object.value(forKey: uniqueKeyPath.propertyName) != nil
             }
+            let mirror = Mirror(reflecting: data)
+            mirror.children.forEach { key, value in
+                guard let key,
+                      let propertyName = String(describing: key)
+                    .split(separator: ".")
+                    .last
+                else { return }
+                object?.setValue(
+                    value,
+                    forKey: String(propertyName)
+                )
+            }
+            if container.viewContext.hasChanges {
+                do {
+                    try container.viewContext.save()
+                } catch {
+                    throw error
+                }
+            }
+        } catch {
+            throw error
         }
     }
     
-    public func delete(data: some CoreDataStorable) {
-        guard let entity = NSEntityDescription.entity(
-            forEntityName: "\(type(of: data))MO",
-            in: container.viewContext
-        )
-        else { return }
-        let object = NSManagedObject(
-            entity: entity,
-            insertInto: container.viewContext
-        )
-        let mirror = Mirror(reflecting: data)
-        mirror.children.forEach { key, value in
-            guard let key,
-            let propertyName = String(describing: key)
-                .split(separator: ".")
-                .last
+    public func delete<T: CoreDataStorable, U>(
+        data: T,
+        uniqueKeyPath: KeyPath<T, U>
+    ) throws {
+        do {
+            let mo = try fetchMO(type: type(of: data))
+            guard let object = mo.first(where: { object in
+                object.value(forKey: uniqueKeyPath.propertyName) != nil
+            })
             else { return }
-            object.setValue(
-                value,
-                forKey: String(propertyName)
-            )
+            container.viewContext.delete(object)
+        } catch {
+            throw error
         }
-        container.viewContext.delete(object)
+    }
+    
+    private func fetchMO<T: CoreDataStorable>(
+        type: T.Type
+    ) throws -> [NSManagedObject] {
+        let request = NSFetchRequest<NSManagedObject>(
+            entityName: "\(type)MO"
+        )
+        do {
+            return try self.container.viewContext.fetch(request)
+        } catch {
+            throw error
+        }
     }
 }
