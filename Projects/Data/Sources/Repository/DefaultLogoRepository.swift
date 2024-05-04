797efcb4097cca959e46047246dbe28ce08406f7
Geonseob Kim <109283556+gnksbm@users.noreply.github.com>
2024-05-05 02:29:22 +0900

    [Feat] LogoRepository 구현

---
+++ Projects/Data/Sources/Repository/DefaultLogoRepository.swift
@@ -0,0 +1,33 @@
+//
+//  DefaultLogoRepository.swift
+//  Data
+//
+//  Created by gnksbm on 5/5/24.
+//  Copyright © 2024 GeonSeobKim. All rights reserved.
+//
+
+import UIKit
+
+import Core
+import Domain
+import Networks
+
+import RxSwift
+
+public final class DefaultLogoRepository: LogoRepository {
+    @Injected(NetworkService.self)
+    private var networkService: NetworkService
+    
+    public init() { }
+    
+    public func fetchLogo(request: LogoRequest) -> Observable<LogoResponse> {
+        let endPoint = LogoEndPoint(request: request)
+        return networkService.requestWithCache(endPoint: endPoint)
+            .map { data in
+                LogoResponse(
+                    ticker: request.ticker,
+                    image: UIImage(data: data)
+                )
+            }
+    }
+}
---
+++ Projects/Domain/Sources/Repository/LogoRepository.swift
@@ -0,0 +1,17 @@
+//
+//  LogoRepository.swift
+//  Domain
+//
+//  Created by gnksbm on 5/5/24.
+//  Copyright © 2024 GeonSeobKim. All rights reserved.
+//
+
+import UIKit
+
+import RxSwift
+
+public protocol LogoRepository {
+    func fetchLogo(
+        request: LogoRequest
+    ) -> Observable<LogoResponse>
+}
