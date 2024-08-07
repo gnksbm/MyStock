97fe0ff7fc816d1b3d0c51b7e064c374c5dc73a0
Geonseob Kim <109283556+gnksbm@users.noreply.github.com>
2024-08-07 16:40:05 +0900

    [Feat] BaseViewController구현

---
+++ Projects/FeatureDependency/Sources/BaseViewController.swift
@@ -0,0 +1,34 @@
+//
+//  BaseViewController.swift
+//  Core
+//
+//  Created by gnksbm on 8/7/24.
+//  Copyright © 2024 GeonSeobKim. All rights reserved.
+//
+
+import UIKit
+
+import ReactorKit
+import RxSwift
+
+open class BaseViewController
+<ReactorType: Reactor>: UIViewController, View {
+    public var disposeBag = DisposeBag()
+    
+    public init() {
+        super.init(nibName: nil, bundle: nil)
+    }
+    
+    @available(*, unavailable)
+    required public init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    final public func bind(reactor: ReactorType) {
+        bindAction(reactor: reactor)
+        bindState(reactor: reactor)
+    }
+    
+    open func bindAction(reactor: ReactorType) { }
+    open func bindState(reactor: ReactorType) { }
+}
